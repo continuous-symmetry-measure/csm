404745df6908d52859cad0caf32592bb36bf239f
Itay Zandbank <zmbq@platonix.com>
2014-11-02 15:13:00 +0200

    Separate the mathematical utils from mainrot to math_utils .

---
+++ CSM/math_utils.h
@@ -0,0 +1,19 @@
+/*
+ * Some utility mathematical functions.
+ *
+ * Extracted from mainRot.cpp during the 2014 reorganization.
+ * Created by Itay Zandbank
+ */
+
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+#include "molecule.h"
+
+void normalize(double **coords, Molecule *m);
+double Magnitude(double *Point1, double *Point2);
+double computeDistanceFromLine(double *Point, double *LineStart, double *LineEnd);
+double findMedian(double arr[], int n);
+
+
+#endif
