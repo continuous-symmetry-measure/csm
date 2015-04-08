ec08dc812ed6d382e902056fe3ab8eb9f330953e
Itay Zandbank <zmbq@platonix.com>
2015-04-09 02:07:19 +0300

    Add the Ubuntu libraries

--- PythonCSM/CPP_wrapper/setup.py
+++ PythonCSM/CPP_wrapper/setup.py
@@ -13,7 +13,9 @@ setup(
             include_dirs=['../../CSM'],
 #            library_dirs=['../../openbabel-files/Windows/lib/x64/Release', '../../CSM/cmake/Release', r'd:\boost\1_57_0\lib64-msvc-10.0'],
 #            libraries=['openbabel-2', 'csmlib'],
+#            libraries=['openbabel', 'csmlib', 'boost_log-mt', 'boost_log_setup-mt', 'boost_system-mt', 'boost_thread-mt', 'boost_filesystem-mt', 'boost_date_time-mt', 'pthread'],
+            libraries=['openbabel', 'csmlib', 'boost_log', 'boost_log_setup', 'boost_system', 'boost_thread', 'boost_filesystem', 'boost_date_time', 'pthread'],
             library_dirs=['../../openbabel-files/unix/lib', '../../CSM/cmake'],
-            libraries=['openbabel', 'csmlib', 'boost_log-mt', 'boost_log_setup-mt', 'boost_system-mt', 'boost_thread-mt', 'boost_filesystem-mt', 'boost_date_time-mt', 'pthread'],
+	    extra_compile_args=['-fPIC'],
         )])
 )
