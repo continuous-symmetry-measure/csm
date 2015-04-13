e701fd9329f0eafffbe05c15dd71482c385ee3f4
Yael Khalman <khalmanyael@gmail.com>
2015-04-13 15:20:45 +0300

    Start parsing arguments in Python

--- PythonCSM/arguments.py
+++ PythonCSM/arguments.py
@@ -1,8 +1,15 @@
 """
 Parse the CSM command line arguments.
 
+        Example:  CSM c2 molecule.xyz output.txt
+        c2 is the type, molecule.xyz is the input_file, output.txt is the output file
+
+        CSM s4 molecule.xyz output.txt -ignoreSym -useperm file.perm
+
+        CSM s4     <--- Fails - not enough arguments
+
 		printf("Usage: %s <type> input_file output_file [-options]\n",)
-		printf("type is one of: cs, ci, cn, sn (n is replaced by the number)\n");
+		printf("type is one of: cs, ci, cn, sn (n is replaced by the number between 2 and 9: c2. ... c9, s2, ... s9)\n");
 		printf("Available options are:\n");
 		printf("-ignoreHy - ignore Hydrogen atoms in computations\n");
 		printf("-removeHy - remove Hydrogen atoms in computations,\n");
@@ -31,14 +38,43 @@ Parse the CSM command line arguments.
 		printf("-help - print this help file\n");
 
 """
+from argparse import ArgumentParser
+
 __author__ = 'zmbq'
 
 def create_parser():
-    # Create the argparse parser
-    pass
+    parser = ArgumentParser()
+
+    # The first three positional arguments
+    parser.add_argument('type', choices=('c2', 'c3', 'c4', 'c8', 's2', 's3', 's4', 's8', 'cs', 'ci'),
+                        help='The type of operation')
+    parser.add_argument('input', help='Input file')
+    # Add output
+
+    # Optional arguments (their names start with --
+    parser.add_argument('--ignoreHy', action='store_true', default=False, help='Ignore Hydrogen atoms in computations')
+    parser.add_argument('--sn_max', type=int, help='The maximal sn to try, relevant only for chirality')
+    return parser
 
 if __name__=='__main__':
+    # This code runs when you execute the script from PyCharm.
     parser = create_parser()
-    args = ['c2', 'abc.xyz', 'abc.output', '--useperm']
+
+    # args = ['--help']
+    args = ['c2', 'abc.xyz', 'abc.output', '--useperm', 'permfile.dat']
+    args=['c4', 'abc.xyz', '--ignoreHy', '--sn_max', '5']
+    result = parser.parse_args()  # Call the parse
+
+    print(result.type)
+    print(result.input)
+    print("Ignore Hydrogen: ", result.ignoreHy)
+
+    if result.sn_max:
+        print("sn_max: %d" % result.sn_max)
+    else:
+        print("No sn_max specified")
+
+    # Parsed arguments are in 'parser'
+    print("Hello from arguments.py")
 
 
