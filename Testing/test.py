c6249eabdfc8e62e9f6ee26a5af904c2c7a1944c
Yael Khalman <khalmanyael@gmail.com>
2015-08-11 10:14:02 +0300

    new chirality tests added.

--- Testing/test.py
+++ Testing/test.py
@@ -46,11 +46,24 @@ class CheckCSMOutput(TestCase):
     def test_12(self):
         self.one_test('test12')"""
 
+    def test_15(self):
+        self.one_test('test15')
+
     def test_16(self):
         self.one_test('test16')
 
     def test_17(self):
         self.one_test('test17')
+
+    def test_20(self):
+        self.one_test('test20')
+
+    def test_21(self):
+        self.one_test('test21')
+
+    # in test 22 the resulting permutation and direction might be different (?) - see output files in test22 folder
+    def test_22(self):
+        self.one_test('test22')
 
 
 class LongCSMCheckOutput(TestCase):
@@ -62,10 +75,6 @@ class LongCSMCheckOutput(TestCase):
 
     def test_14(self):
         self.one_test('test14')
-
-    """Test 15 is takes a lot of time"""
-    def test_15(self):
-        self.one_test('test15')
 
     """def test_18(self):
         self.one_test('test18')"""
