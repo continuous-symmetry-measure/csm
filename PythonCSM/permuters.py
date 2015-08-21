270fcf9d6c8ed6d97f31708cff7466c62774f175
Itay Zandbank <zmbq@platonix.com>
2015-08-22 00:42:02 +0300

    Comments.

--- PythonCSM/permuters.py
+++ PythonCSM/permuters.py
@@ -54,6 +54,13 @@ def display_perms(prefix, perms):
 # cycle_structs: A list of cycles in a permutation: [[0], [1, 3, 4], [2], ...]
 
 def get_cycle_structs(perm_size, cycle_sizes):
+    """
+    Generates a list of cycles in a permutation. The cycles cover the entire permutation, and are only of sizes in cycle_sizes
+    :param perm_size: Permutation size
+    :param cycle_sizes: Allowed cycle sizes
+    :return: A generator of a list of cycles covering the entire permtutation
+
+    """
     def generate(cycles, left):
         len_left = len(left)
         if len_left == 0:
@@ -69,17 +76,23 @@ def get_cycle_structs(perm_size, cycle_sizes):
                 new_left = [item for item in left if item not in cycle]
                 yield from generate(cycles+[cycle], new_left)
 
+    if not 1 in cycle_sizes:
+        raise ValueError("1 must be a valid cycle size")
     yield from generate([], list(range(perm_size)))
 
 def all_circles(elements):
-    """ Returns all the circles (full cycles) of elements (e0->e1->e2->e3...->e0) and the trivial cycle """
+    """ Generates all the circles (permutations that are full cycles) of 'elements'
+
+    If elements are [A,B,C], returns (B,C,A) and (C,A,B)
+    """
 
     def all_circle_indices(size):
+        """ Returns the indices of the cycle, that will later be applied to elements """
         # To compute a full cycle of length n, we take a permutation p of size n-1 and
         # create the cycle like so: 0 goes to p[0], p[0] to p[1], p[1] to p[2] and so on, until p[n-1] goes back to 0
         # For this to work, p needs to be a permutation of 1..n-1.
         #
-        # For example, size = 3 we have p=(1,2) for the cycle 0->1->2->0 and p=(2,1) for the cycle 0->2->1->0
+        # For example, size = 3 we have p=(1,2) for the circle 0->1->2->0 and p=(2,1) for the circle 0->2->1->0
         trivial = tuple(range(1, size))
         for cycle in itertools.permutations(trivial):
             full_cycle = [0] * size
@@ -95,13 +108,19 @@ def all_circles(elements):
         yield circle
 
 def all_perms_from_cycle_struct(perm_size, cycle_struct):
+    """
+    Generates all permutations with a specific cycle structure
+    :param perm_size: Permutation size
+    :param cycle_struct: Cycle structure (list of cycles, each cycle is itself a list of its indices)
+    :return: Generator that generates all the permutations
+    """
     trivial = list(range(perm_size))
 
     def generate(perm, cycle_struct, cycles_left):
+        # Goes over all the circles of the first cycle, fixes each circle and
+        # recursively generates the circles of the rest of the cycles
         if not cycles_left:
-            if perm != trivial:
-                #print("%s: %s" % (cycle_struct, perm))
-                yield tuple(perm)
+            yield tuple(perm)
             return
 
         cycle = cycles_left[0]
@@ -109,9 +128,10 @@ def all_perms_from_cycle_struct(perm_size, cycle_struct):
         if len(cycle) > 1:
             start_perm = perm[:]
             for circle in all_circles(cycle):
+                # Apply each circle
                 for i in range(len(circle)):
                     perm[cycle[i]] = circle[i]
-                yield from generate(perm, cycle_struct, cycles_left)
+                yield from generate(perm, cycle_struct, cycles_left) # Apply the rest of the circles
                 perm = start_perm[:]
         else:
             yield from generate(perm, cycle_struct, cycles_left)
@@ -119,15 +139,22 @@ def all_perms_from_cycle_struct(perm_size, cycle_struct):
     yield from generate(trivial[:], cycle_struct, cycle_struct)
 
 
-def get_permutations(perm_size, group_size, add_groups_of_two):
-    cycle_lengths = {1, group_size}
-    if add_groups_of_two:
+def get_permutations(perm_size, cycle_size, add_cycles_of_twp):
+    """
+    Generates all permutations of size and cycle sizes.
+    :param perm_size: Permutation size
+    :param cycle_size: Cycle size
+    :param add_cycles_of_twp: When true, cycles of size of two are legal
+    :return: Generator for all the permutations
+    """
+    cycle_lengths = {1, cycle_size}
+    if add_cycles_of_twp:
         cycle_lengths.add(2)
     cycle_lengths = sorted(cycle_lengths)
 
-    yield tuple(range(perm_size)) # The identity permutation first
     for cycle_struct in get_cycle_structs(perm_size, cycle_lengths):
-        yield from all_perms_from_cycle_struct(perm_size, cycle_struct)
+        # Loop over all cycle structures, which are of the allowed lengths covering the entire permutation
+        yield from all_perms_from_cycle_struct(perm_size, cycle_struct) # Return all permutations for each cycle structure
 
 
 def compare(perm_size, group_size, add_groups_of_two):
@@ -177,7 +204,11 @@ def compare(perm_size, group_size, add_groups_of_two):
             print(colorama.Fore.LIGHTRED_EX + "   NOT IN PYTHON" + colorama.Fore.RESET, end='')
         print()
 
+def timeit(perm_size, group_size, add_groups_of_two):
+    generator = get_permutations(perm_size, group_size, add_groups_of_two)
+
 #print(list(all_circles((2,3))))
-compare(12, 5, True)
+
+compare(11, 4, True)
 # print(list(all_circles((0,1,2,3))))
 # print (list(all_perms_from_cycle_struct(4, [[0,1], [2,3]])))
