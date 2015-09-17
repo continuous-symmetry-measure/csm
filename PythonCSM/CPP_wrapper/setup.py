b76dcb3ba049de217cc6e05c7c7d100bab3494b4
Itay Zandbank <zmbq@platonix.com>
2015-09-17 09:54:26 +0300

    Don't require OpenBabel in the Cython wrapper.

--- PythonCSM/CPP_wrapper/setup.py
+++ PythonCSM/CPP_wrapper/setup.py
@@ -17,16 +17,16 @@ extra_compile_args = []
 extra_link_args = []
 if sys.platform == 'win32':
     library_dirs = ['../../openbabel-files/Windows/lib/x64/Release', '../../CSM/cmake/Release', BOOST_ROOT]
-    libraries = ['openbabel-2', 'csmlib']
+    libraries = ['csmlib']
     # extra_compiler_args = ['/Zi', '/Od']  # Debug info, no optimization
     extra_link_args = ['/debug']
 elif sys.platform in ['linux', 'linux2']:
     library_dirs = ['../../openbabel-files/unix/lib', '../../CSM/cmake']
-    libraries = ['openbabel', 'csmlib', 'boost_log', 'boost_log_setup', 'boost_system', 'boost_thread', 'boost_filesystem', 'boost_date_time', 'pthread']
+    libraries = ['csmlib', 'boost_log', 'boost_log_setup', 'boost_system', 'boost_thread', 'boost_filesystem', 'boost_date_time', 'pthread']
     extra_compile_args = ['-fPIC']
 elif sys.platform == 'darwin':
     library_dirs = ['../../openbabel-files/unix/lib', '../../CSM/cmake']
-    libraries = ['openbabel', 'csmlib', 'boost_log-mt', 'boost_log_setup-mt', 'boost_system-mt', 'boost_thread-mt', 'boost_filesystem-mt', 'boost_date_time-mt', 'pthread']
+    libraries = ['csmlib', 'boost_log-mt', 'boost_log_setup-mt', 'boost_system-mt', 'boost_thread-mt', 'boost_filesystem-mt', 'boost_date_time-mt', 'pthread']
 
 setup(
     ext_modules=cythonize(
