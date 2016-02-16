874c3ea18cef7b2d85561dc60c265676af9df634
Devora Witty <devorawitty@gmail.com>
2016-02-16 13:33:16 +0200

    caching of theta and perm

--- PythonCSM/calculations/ref_plane.py
+++ PythonCSM/calculations/ref_plane.py
@@ -14,16 +14,28 @@ from numpy.polynomial import Polynomial
 # logger = logging.getLogger("csm")
 
 
+def apply_perm(curr_perm, perm, size):
+    return [perm[curr_perm, j] for j in range(size)]
+
+
 def calc_ref_plane(molecule, perm, op_order, op_type):
     size = len(molecule.atoms)
     is_improper = op_type != 'CN'
     is_zero_angle = op_type == 'CS'
 
-    #logger.debug('************* calc_ref_plane Python ************************')
-    #logger.debug('Permutation is ' + str(perm))
+    #pre-caching:
+    theta=np.zeros(op_order)
+    if not is_zero_angle:
+        for i in range(1, op_order):
+            theta[i] = 2 * math.pi * i / op_order
 
-#    logger.debug("calcrefplane atoms:")
-#    logger.debug([atom.pos for atom in molecule.atoms])
+    perms=np.empty([op_order, size], dtype=np.int)
+    perms[0]=[i for i in range(size)]
+    for i in range(1, op_order):
+        perms[i]=[perm[perms[i-1][j]]for j in range(size)]
+
+
+
 
     # For all k, 0 <= k < size, Q[k] = column vector of x_k, y_k, z_k (position of the k'th atom)
     # - described on the first page of the paper
@@ -38,24 +50,18 @@ def calc_ref_plane(molecule, perm, op_order, op_type):
         # A is calculated according to formula (17) in the paper
         # B is calculated according to formula (12) in the paper
 
-        cur_perm = [i for i in range(size)]
         A = np.zeros((3, 3,))
         B = np.zeros((1, 3))  # Row vector for now
 
         # compute matrices according to current perm and its powers (the identity does not contribute anyway)
         for i in range(1, op_order):
-            if is_zero_angle:
-                theta = 0.0
-            else:
-                theta = 2 * math.pi * i / op_order
-
             if is_improper and (i % 2):
-                multiplier = -1 - math.cos(theta)
+                multiplier = -1 - math.cos(theta[i])
             else:
-                multiplier = 1 - math.cos(theta)
+                multiplier = 1 - math.cos(theta[i])
 
             # The i'th power of the permutation
-            cur_perm = [perm[cur_perm[j]] for j in range(size)]
+            cur_perm = perms[i]
 
             # Q_ is Q after applying the i'th permutation on atoms (Q' in the article)
             # Q_ = [Q[p] for p in cur_perm]  # Q'
@@ -63,7 +69,7 @@ def calc_ref_plane(molecule, perm, op_order, op_type):
             # A_intermediate is calculated according to the formula (5) in the paper
             for k in range(size):
                 A = A + multiplier * ((Q[cur_perm[k]] @ Q[k].T) + (Q[k] @ Q[cur_perm[k]].T))
-                B = B + math.sin(theta) * np.cross(Q[k].T, Q[cur_perm[k]].T)
+                B = B + math.sin(theta[i]) * np.cross(Q[k].T, Q[cur_perm[k]].T)
 
         return A, B.T  # Return B as a column vector
 
@@ -180,7 +186,6 @@ def calc_ref_plane(molecule, perm, op_order, op_type):
 
     def calculate_csm():
         # initialize identity permutation
-        cur_perm = [i for i in range(size)]
 
         # CSM is computed according to formula (15) in the paper with some changes according to CPP code:
         #  - lambda_max is divided by 2 (this appears only in CPP code)
@@ -188,21 +193,16 @@ def calc_ref_plane(molecule, perm, op_order, op_type):
         #   made here but in the normalizations section
         csm = 1.0
         for i in range(1, op_order):
-            # This can be more efficient - save results of matrix computation
-            if is_zero_angle:
-                theta = 0.0
-            else:
-                theta = 2 * math.pi * i / op_order
             dists = 0.0
 
             # i'th power of permutation
-            cur_perm = [perm[cur_perm[j]] for j in range(size)]
+            cur_perm = perms[i]
 
             # Q_ = [Q[cur_perm[i]] for i in range(size)]
 
             for k in range(size):
                 dists += Q[k].T @ Q[cur_perm[k]]
-            csm += math.cos(theta) * dists
+            csm += math.cos(theta[i]) * dists
 
         # logger.debug("csm=%lf lambda_max=%lf m_max_B=%lf" % (csm, lambda_max, m_max_B))
         # logger.debug("dir: %lf %lf %lf" % (dir[0], dir[1], dir[2]))
