a8edb13dc457984031905c8fcb2ab9e8cf0eeb5c
Devora Witty <devorawitty@gmail.com>
2016-05-03 16:28:17 +0300

    created class DistanceMatrix that uses a numpy array to store distances, and changed algorithm of perm_builder to match

--- python/csm/calculations/approx_calculations.py
+++ python/csm/calculations/approx_calculations.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import logging
+from collections import namedtuple
 from csm.calculations.constants import MINDOUBLE, MAXDOUBLE
 from csm.fast import CythonPermuter, SinglePermPermuter, TruePermChecker, PQPermChecker, CythonPIP
 from csm.fast import external_get_eigens as cppeigen
@@ -27,7 +28,6 @@ def find_best_perm(op_type, op_order, molecule, detect_outliers):
         for dir in dirs:
             # find permutation for this direction of the symmetry axis
             perm = estimate_perm(op_type, op_order, molecule, dir)
-
 
             # solve using this perm until it converges:
             best_for_this_dir = interim_results = csm_operation(op_type, op_order, molecule, SinglePermPermuter,
@@ -80,7 +80,6 @@ def normalize_dir(dir):
     dir /= norm
     return dir
 
-
 def dir_fit(positions):
     # get vector of the average position
     sum = np.einsum('ij->j', positions)
@@ -104,8 +103,29 @@ def dir_fit(positions):
 
     return dirs
 
-
 def dirs_without_outliers(dirs, positions, op_type):
+    def compute_distance_from_line(group_avg_point, test_dir_end):
+        def magnitude(point1, point2):
+            vec = point2 - point1
+            return math.sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2])
+
+        def get_U(Point, LineStart, LineEnd, LineMag):
+            return (((Point[0] - LineStart[0]) * (LineEnd[0] - LineStart[0])) +
+                    ((Point[1] - LineStart[1]) * (LineEnd[1] - LineStart[1])) +
+                    ((Point[2] - LineStart[2]) * (LineEnd[2] - LineStart[2]))) / (LineMag * LineMag)
+
+        def get_intersect(LineStart, LineEnd, U):
+            Intersection = [0, 0, 0]
+            Intersection[0] = LineStart[0] + U * (LineEnd[0] - LineStart[0]);
+            Intersection[1] = LineStart[1] + U * (LineEnd[1] - LineStart[1]);
+            Intersection[2] = LineStart[2] + U * (LineEnd[2] - LineStart[2]);
+            return Intersection
+
+        linestart = [0, 0, 0]
+        linemag = magnitude(test_dir_end, linestart)
+        U = get_U(group_avg_point, linestart, test_dir_end, linemag)
+        intersection = get_intersect(linestart, test_dir_end, U)
+        return magnitude(group_avg_point, intersection)
     print("======================detecting outliers============================")
     more_dirs = []
     for dir in dirs:
@@ -135,31 +155,6 @@ def dirs_without_outliers(dirs, positions, op_type):
         outliers_dirs = dir_fit(with_outliers_removed)
         more_dirs += list(outliers_dirs)
     return np.array(more_dirs)
-
-
-def compute_distance_from_line(group_avg_point, test_dir_end):
-    def magnitude(point1, point2):
-        vec = point2 - point1
-        return math.sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2])
-
-    def get_U(Point, LineStart, LineEnd, LineMag):
-        return (((Point[0] - LineStart[0]) * (LineEnd[0] - LineStart[0])) +
-                ((Point[1] - LineStart[1]) * (LineEnd[1] - LineStart[1])) +
-                ((Point[2] - LineStart[2]) * (LineEnd[2] - LineStart[2]))) / (LineMag * LineMag)
-
-    def get_intersect(LineStart, LineEnd, U):
-        Intersection = [0, 0, 0]
-        Intersection[0] = LineStart[0] + U * (LineEnd[0] - LineStart[0]);
-        Intersection[1] = LineStart[1] + U * (LineEnd[1] - LineStart[1]);
-        Intersection[2] = LineStart[2] + U * (LineEnd[2] - LineStart[2]);
-        return Intersection
-
-    linestart = [0, 0, 0]
-    linemag = magnitude(test_dir_end, linestart)
-    U = get_U(group_avg_point, linestart, test_dir_end, linemag)
-    intersection = get_intersect(linestart, test_dir_end, U)
-    return magnitude(group_avg_point, intersection)
-
 
 def dirs_orthogonal(dirs):
     added_dirs = list(dirs)
@@ -198,6 +193,43 @@ class distance_record:
     def __str__(self):
         return "distance" + str(self.row) + "," + str(self.col) + ": " + str(self.distance)
 
+Distance= namedtuple('Distance', 'from_val to_val distance')
+
+class DistanceMatrix:
+    def __init__(self, group):
+        self.np_distances= np.ones((len(group), len(group))) * MAXDOUBLE
+        self._np_delete = np.copy(self.np_distances[0])
+        #self.list_distances=[]
+
+    def add(self, from_val, to_val, distance=MAXDOUBLE):
+        self.np_distances[from_val][to_val]=distance
+        #dist=Distance(from_val=from_val, to_val=to_val, distance=distance)
+        #self.list_distances.append(dist)
+
+    def sort(self):
+        pass#self.list_distances = newlist = sorted(self.list_distances, key=lambda x: x.distance)
+
+    def remove(self, from_val, to_val):
+        self.np_distances[:,to_val]=self._np_delete
+        self.np_distances[from_val,:]=self._np_delete
+
+    def get_min_val(self):
+        a=self.np_distances
+        from_val = int(np.argmin(a) / len(a[0]))
+        to_val= int(np.argmin(a) % len(a[0]))
+        return (from_val, to_val)
+
+    def get_next_in_cycle(self, from_val, constraints):
+        searched_row=self.np_distances[from_val]
+        indices= np.argsort(searched_row)
+        i=0
+        if constraints:
+            while indices[i] in constraints:
+                i+=1
+        to_val=indices[i]
+        return (from_val, to_val)
+
+
 
 def estimate_perm(op_type, op_order, molecule, dir):
     # this is a naive implementation, which works well with the naive c++ build_perm
@@ -214,25 +246,92 @@ def estimate_perm(op_type, op_order, molecule, dir):
 
     # permutation creation is done by group:
     for group in molecule.equivalence_classes:
-        if len(group) == -1:
+        if len(group) == 1:
             perm[group[0]] = group[0]
-            # np_distances[group[0]][group[0]]=0
-
+            #print(group[0], 0, group[0], group[0], sep=", ")
         else:
             # measure all the distances between all the points in X to all the points in Y
-            distances = list()
-            for i in group:
-                for j in group:
-                    a = rotated[i]
-                    b = molecule.Q[j]
+            distances = DistanceMatrix(group)#list()
+            for i in range(len(group)):
+                for j in range(len(group)):
+                    a = rotated[group[i]]
+                    b = molecule.Q[group[j]]
                     distance = np.linalg.norm(a - b)
-                    distances.append(distance_record(j, i, distance))
+                    distances.add(j,i,distance)
+                    #distances.append(distance_record(j, i, distance))
 
             perm = perm_builder(op_type, op_order, group, distances, perm)
 
     return perm
 
-def perm_builder(op_type, op_order, group, distances, perm):
+def perm_builder(op_type, op_order, group, distance_matrix, perm):
+    group_id=np.min(group)
+    left=len(group)
+    while left>=op_order:
+        (from_val, to_val)=distance_matrix.get_min_val()
+        perm[group[from_val]]=group[to_val]
+        distance_matrix.remove(from_val, to_val)
+        #print(group_id, left, group[from_val], group[to_val], sep=", ")
+        left-=1
+        if from_val==to_val: #cycle length 1 completed
+            continue
+        #otherwise, build cycle:
+        cycle_head=from_val
+        cycle_length=1
+        cycle_done=False
+
+        while not cycle_done:
+            constraints= set()
+            constraints.add(cycle_head) #prevents too-short cycle
+            if cycle_length== op_order-1:
+                from_val = to_val
+                to_val = cycle_head
+                cycle_done = True
+            else:
+                constraints.add(to_val)#prevent dead-end cycle
+                constraints.add(from_val)  # prevent dead-endloop
+                if (op_type=='SN' or op_order==2) and cycle_length<2:
+                    constraints.remove(cycle_head) #remove cycle_head from constraints (also removes from_val)
+
+                (next_from_val, next_to_val)=distance_matrix.get_next_in_cycle(to_val, constraints)
+                if next_to_val==from_val: #cycle length 2 - only possible if above if(op_type==SN... was True
+                    cycle_done=True
+                (from_val, to_val)=(next_from_val, next_to_val)
+
+            perm[group[from_val]]=group[to_val]
+            distance_matrix.remove(from_val, to_val)
+            #print(group_id, left, group[from_val], group[to_val], sep=", ")
+            left-=1
+            cycle_length+=1
+        pass
+
+    #for remaining pairs or singles, simply go through them and set them
+    while True:
+        (from_val, to_val) = distance_matrix.get_min_val()
+        if perm[group[from_val]] != -1: #we've finished with the group
+            break
+        if op_type=='SN':
+            perm[group[from_val]] = group[to_val]
+            perm[group[to_val]] = group[from_val]
+            distance_matrix.remove(from_val, to_val)
+            distance_matrix.remove(to_val, from_val)
+            #print(group_id, left, group[from_val], group[to_val], sep=", ")
+            #left-=1
+            #if from_val!=to_val:
+            #    print(group_id, left, group[to_val], group[from_val], sep=", ")
+            #    left-=1
+        else:
+            perm[group[from_val]]=group[from_val]
+            distance_matrix.remove(from_val, from_val)
+            #left -= 1
+            #print(group_id, left, group[from_val], group[from_val], sep=", ")
+    return perm
+
+
+
+
+
+def cpp_perm_builder(op_type, op_order, group, distances, perm):
     # this is intended to serve as a copy-paste of the (buggy) c++ algorithm
     left = len(group)
     used = np.zeros(len(perm))  # array to keep track of which values have already been placed
@@ -247,30 +346,28 @@ def perm_builder(op_type, op_order, group, distances, perm):
             continue
         # If we do not have enought to fill cycles, set all remaining items to themselves
         if left <= 1 or (op_type == 'CN' and left < op_order):
-            if perm[row] == -1:  #this is redundant as it has already been checked
-                perm[row] = row
-                used[row] = 1
-                left -= 1
+            #if perm[row] == -1:  #this is redundant as it has already been checked
+            perm[row] = row
+            used[row] = 1
+            left -= 1
 
         elif op_order == 2:
             # Special treatment - only size  1 and two  orbits are allowed
             # If both elements  are not yet set, use the element.
-            if perm[row] == -1 and perm[col] == -1:  # and used[row] == used[col] == 0:  #this is redundant as it has already been checked
-                perm[row] = col
-                perm[col] = row
-                used[row] = used[col] = 1
-                if row == col:
-                    left -= 1
-                else:
-                    left -= 2
-        else:
-            # we now want to complete an orbit
-            if perm[row] == -1 and used[col] == 0: #this is redundant as it has already been checked
-                perm[row] = col
-                used[col] = 1
+            #if perm[row] == -1 and perm[col] == -1:  # and used[row] == used[col] == 0:  #this is redundant as it has already been checked
+            perm[row] = col
+            perm[col] = row
+            used[row] = used[col] = 1
+            if row == col:
                 left -= 1
             else:
-                continue
+                left -= 2
+        else:
+            # we now want to complete an orbit
+            #if perm[row] == -1 and used[col] == 0: #this is redundant as it has already been checked
+            perm[row] = col
+            used[col] = 1
+            left -= 1
             # if this is an orbit of size one, "cycle" is complete, so...
             if row == col:
                 continue
