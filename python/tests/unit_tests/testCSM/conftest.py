838e50589f2d53677ab9e10720773b32e0c5c592
devora <devorawitty@gmail.com>
2016-09-20 12:08:04 +0300

    tests cont

--- python/tests/unit_tests/testCSM/conftest.py
+++ python/tests/unit_tests/testCSM/conftest.py
@@ -3,29 +3,49 @@ import json
 from tests.utils.run_test import Expected
 
 def pytest_addoption(parser):
-    parser.addoption("--dir", type=str)
+    parser.addoption("--dir", action="append", default=[],
+        help="list of test directories to pass to test functions")
+    parser.addoption("--parent", action="append", default=[],
+                     help="list of parent directories, whose child directories will be passed to test functions")
 
 def pytest_generate_tests(metafunc):
-    mydir=metafunc.config.option.dir
-    inputjson=os.path.join(mydir, 'input.json')
-    outputjson=os.path.join(mydir, 'output.json')
-    molecule_folder = os.path.join(mydir, 'molecules')
+    dirs=metafunc.config.option.dir[:]
+    for test_folder in metafunc.config.option.parent:
+        for folder in os.listdir(test_folder):
+            mydir=os.path.join(test_folder,folder)
+            if os.path.isdir(mydir):
+                dirs.append(mydir)
+    params=get_run_tuples(dirs)
 
+
+    if 'args' in metafunc.fixturenames:
+        metafunc.parametrize("args, molecule, expected, equiv", params)
+
+
+
+def get_run_tuples(dirs):
     params=[]
-
-    with open(inputjson) as f:
-        in_dict=json.load(f)
-    with open(outputjson) as f:
-        output_dict = json.load(f)
-
-    for key in in_dict["runs"]:
-        for molecule in os.listdir(molecule_folder):
-            molfile=os.path.join(molecule_folder, molecule)
-            mol_index = molecule.split(".")[0]
-            e = output_dict[key][mol_index]
-            expected = Expected(e)
-            my_tuple=(in_dict["runs"][key], molfile, expected)
-            params.append(my_tuple)
+    for mydir in dirs:
+        inputjson=os.path.join(mydir, 'input.json')
+        outputjson=os.path.join(mydir, 'output.json')
+        molecule_folder = os.path.join(mydir, 'molecules')
 
 
-    metafunc.parametrize("args, molecule, expected", params)
+
+        with open(inputjson) as f:
+            in_dict=json.load(f)
+        with open(outputjson) as f:
+            output_dict = json.load(f)
+
+
+        for key in in_dict["runs"]:
+            for molecule in os.listdir(molecule_folder):
+                molfile=os.path.join(molecule_folder, molecule)
+                mol_index = molecule.split(".")[0]
+                e = output_dict[key][mol_index]
+                expected = Expected(e)
+                my_tuple=(in_dict["runs"][key], molfile, expected, in_dict['equiv_perms'])
+                params.append(my_tuple)
+
+    return params
+
--- python/tests/unit_tests/testCSM/test_CSM.py
+++ python/tests/unit_tests/testCSM/test_CSM.py
@@ -4,11 +4,39 @@ from csm.main.csm_run import run
 from tests.utils.run_test import close_enough
 
 
-
-
-def test_csm(args, molecule, expected):
+def run_test(args, molecule):
     args[1]=molecule
     args[2]=r'D:\UserData\devora\Sources\csm\python\tests\unit_tests\noonecares.txt'
     result=run(args)
+    return result
+
+
+def test_csm(args, molecule, expected, equiv):
+    result=run_test(args, molecule)
     assert close_enough(result.csm, expected.csm)
 
+def test_symmetric_structure(args, molecule, expected, equiv):
+    result=run_test(args, molecule)
+    atom_pos_not_match = []
+    for i in range(len(expected.molecule)):
+        for index in range(3):
+            val1 = expected.symmetric_structure[i][index]
+            val2 = result.symmetric_structure[i][index]
+            if not close_enough(val1, val2):
+                atom_pos_not_match.append((str(i), index, val1, val2))
+    assert(len(atom_pos_not_match)==0)
+
+def test_perm(args, molecule, expected, equiv):
+    result=run_test(args, molecule)
+    symm=args[0]
+
+    exp_p=expected.perm
+    res_p=result.perm
+    if expected.perm != result.perm:
+        if symm in equiv:
+            for pair in equiv[symm]:
+                if expected.perm in pair and result.perm in pair:
+                    res_p=exp_p
+                    break
+
+    assert(exp_p==res_p)
