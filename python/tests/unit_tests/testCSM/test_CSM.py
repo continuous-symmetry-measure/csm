79b9936881d06e67969d94694e1314d3e0192dd3
Itay Zandbank <itay@chelem.co.il>
2016-10-26 15:37:52 +0300

    Get the tests to run on another computer

--- python/tests/unit_tests/testCSM/test_CSM.py
+++ python/tests/unit_tests/testCSM/test_CSM.py
@@ -2,7 +2,7 @@ import pytest
 import json
 from csm.main.csm_run import run as csmrun
 from tests.utils.run_test import close_enough
-
+import os
 
 class TestClass:
     args=None
@@ -11,32 +11,27 @@ class TestClass:
         args[1] = molecule
         if self.__class__.args !=list(args):
             self.__class__.args = list(args)
-            args[2] = r'D:\UserData\devora\Sources\csm\python\tests\unit_tests\noonecares.txt'
+            args[2] = os.devnull
             result = csmrun(args)
             self.__class__.result=result
         return self.__class__.result
 
     def test_csm(self, args, molecule, expected, equiv):
-        print("CSM")
         result = self.run_test(args, molecule)
-        print("res",result.csm, "exp", expected.csm)
         assert close_enough(result.csm, expected.csm)
 
     def test_symmetric_structure(self, args, molecule, expected, equiv):
-        print("SYMM")
         result=self.run_test(args, molecule)
         atom_pos_not_match = []
         for i in range(len(expected.molecule)):
             for index in range(3):
                 val1 = expected.symmetric_structure[i][index]
                 val2 = result.symmetric_structure[i][index]
-                print("exp",val1,"res", val2)
                 if not close_enough(val1,val2):
                     atom_pos_not_match.append((str(i), index, val1, val2))
         assert(len(atom_pos_not_match)==0)
 
     def test_perm(self, args, molecule, expected, equiv):
-        print("PERM")
         result=self.run_test(args, molecule)
         symm=args[0]
         if symm[0].upper()=="S":
@@ -44,7 +39,6 @@ class TestClass:
 
         exp_p=expected.perm
         res_p=result.perm
-        print("res", res_p, "exp", exp_p)
         if expected.perm != result.perm:
             if symm in equiv:
                 for pair in equiv[symm]:
