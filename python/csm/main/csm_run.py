ad29767c00be4b9b5786180597fea9e8f25bdea2
devorawitty <devorawitty@gmail.com>
2017-06-07 12:28:31 +0300

    BAD MERGE FIX: changed imports in csmrun to fit new structure

--- python/csm/main/csm_run.py
+++ python/csm/main/csm_run.py
@@ -5,7 +5,8 @@ import sys
 import timeit
 from csm.input_output.arguments import get_split_arguments
 from csm.calculations.exact_calculations import exact_calculation, perm_count
-from csm.calculations.approx_calculations import approx_calculation, trivial_calculation
+from csm.calculations.approx.main import approx_calculation
+from csm.calculations.trivial import trivial_calculation
 from csm.calculations import exact_calculations
 from csm.input_output.readers import read_inputs
 from csm.input_output.writers import print_results
