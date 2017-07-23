531d2743a5440a4357257028c1e80a5f765a8724
devorawitty <devorawitty@gmail.com>
2017-07-23 12:32:49 +0300

    added tests related to fragments

--- python/tests/regression_tests/test_bugs.py
+++ python/tests/regression_tests/test_bugs.py
@@ -9,10 +9,9 @@ def test_pdb_with_chains_hetatm_and_nonequal_equivalence_classes():
 
 
     args= "--approx --use-chains --remove-hy --many-chains"
-    '''
-      File "C:\Users\devora.CHELEM\Sources\csm\python\csm\calculations\approx\approximators.py", line 347, in _approximate
-    indexes, group_distance_matrix= self._hungarian_on_groups(group_k, group_m, rotation_mat   )
-  File "C:\Users\devora.CHELEM\Sources\csm\python\csm\calculations\approx\approximators.py", line 386, in _hungarian_on_groups
-    group_distance_matrix[a, b] = distance
-IndexError: index 121 is out of bounds for axis 1 with size 121
-'''
+
+
+#File "C:\Users\devora.CHELEM\Sources\csm\python\csm\calculations\approx\approximators.py", line 347, in _approximate
+#indexes, group_distance_matrix= self._hungarian_on_groups(group_k, group_m, rotation_mat   )
+#File "C:\Users\devora.CHELEM\Sources\csm\python\csm\calculations\approx\approximators.py", line 386, in _hungarian_on_group_group_distance_matrix[a, b] = distance
+#IndexError: index 121 is out of bounds for axis 1 with size 121
--- python/tests/unit_tests/test_read_fragments.py
+++ python/tests/unit_tests/test_read_fragments.py
@@ -16,6 +16,79 @@ r'C:\Users\devora.CHELEM\Sources\csm\test_cases\original\test16\tetralin.mol'
 r'C:\Users\devora.CHELEM\Sources\csm\test_cases\original\test1\AgCu10p1.xyz'
 #cif
 
+
+#mol with fragments, no flags
+def test_should_only_read_first_molecule_without_read_fragments():
+    file = os.path.join(inbal_folder, 'water-6.mol')
+    m = Molecule.from_file(file, format='mol')
+    assert (len(m.chains) == 1)
+    assert (len(m)==3)
+
+#mol with fragments, --use-chains
+#print: no chains
+#How can i test if there is a printout?
+def test_should_only_read_first_molecule_without_read_fragments_ignore_use_chains(caplog):
+    file = os.path.join(inbal_folder, 'water-6.mol')
+    m = Molecule.from_file(file, format='mol', use_chains=True)
+    for record in caplog.records():
+        hi=1
+    assert (len(m.chains) == 1)
+    assert (len(m)==3)
+
+#mol with fragments, --read-fragments
+#print: added use chains automatically
+def test_should_read_molecules_as_chains_with_read_fragments(capsys):
+    file=os.path.join(inbal_folder, 'water-6.mol')
+    m=Molecule.from_file(file, format='mol', read_fragments=True)
+    out, err = capsys.readouterr()
+    assert(len(m.chains)==6)
+    assert(len(m)==18)
+
+#mol with fragments, --use-chains, -read-fragments
+def test_should_read_molecules_as_chains_with_read_fragments():
+    file=os.path.join(inbal_folder, 'water-6.mol')
+    m=Molecule.from_file(file, format='mol', read_fragments=True, use_chains=True)
+    assert(len(m.chains)==6)
+    assert(len(m)==18)
+
+#pdb with endmdl, no flags
+
+#pdb with endmdl, --use-chains
+
+#pdb with endmdl, --read-fragments
+
+#pdb with endmdl, --use-chains, --read-fragments
+
+#pdb with chains(ter), no flags
+
+#pdb with chains(ter), --use-chains
+
+#pdb with chains(ter), --read-fragments
+
+#pdb with chains(ter), --use-chains, --read-fragments
+
+#pdb with chains and endmdl, flags irrelevant:
+
+
+#pdb with endmdl, --ignore-hetatm
+
+#pdb with endmdl, --use-chains, --ignore-hetatm
+
+#pdb with endmdl, --read-fragments, --ignore-hetatm
+
+#pdb with endmdl, --use-chains, --read-fragments, --ignore-hetatm
+
+#pdb with chains(ter), --ignore-hetatm
+
+#pdb with chains(ter), --use-chains, --ignore-hetatm
+
+#pdb with chains(ter), --read-fragments, --ignore-hetatm
+
+#pdb with chains(ter), --use-chains, --read-fragments, --ignore-hetatm
+
+
+
+
 #pdb without chains
 #pdb with chains
 #pdb with hetatm
@@ -62,3 +135,46 @@ def test_pdb_with_chains_hetatm_and_nonequal_equivalence_classes():
 #trivial without use-chains
 #exact with keep-structure
 #normalizations with use-chains, 0-6
+
+#add ignore_hetatms
+
+#scenario: mol file with 3 molecules separated by $$$,  user selected no flags
+#result: a single molecule matching the first molecule in the file
+
+#scenario: mol file with 3 molecules separated by $$$,  user selected --use-chains
+#result: a single molecule matching the first molecule in the file
+
+
+#scenario: mol file with 3 molecules separated by $$$,  user selected --read-fragments
+#result: a single molecule composed of 3 chains, each one matching the atoms described for their respective molecule in the file
+#(currently this scenario raises an error and asks that the user also select --use-chains. since I understood from your email that that's not the desired behavior, I would change this so that inputting --read-fragments automatically selects --use-chains as well)
+#(theoretical alternate result: a single molecule with the atoms from all three molecules and no chains. it sounds like you don't want this)
+
+#scenario: mol file with 3 molecules separated by $$$,  user selected --read-fragments, --use-chains
+#result: a single molecule composed of 3 chains, each one matching the atoms described for their respective molecule in the file
+
+#scenario: mol file with 1 molecule,  user selected --read-fragments, --use-chains
+#result: a single molecule matching the 1 molecule. no chains
+#prints message
+
+#scenario: pdb model that contains chains and hetatms. user selected no flags
+#result: a single molecule with no chains, containing all the atoms marked ATOM and HETATM in the pdb file
+
+#naming chains-- other than chains in pdb, all chains get a oridnal number
+
+#scenario: pdb model that contains chains and hetatms. user selected --use-chains
+#result: a single molecule with chains matching those indicated in the pdb file. should the hetatms also be marked as chains?
+
+#scenario: pdb model that contains chains and hetatms. user selected --use-chains, --read-fragments
+#result: a single molecule with chains matching those indicated in the pdb file, plus hetatms as chains??
+
+#scenario: pdb with two models, no chains. user selected read-fragments
+#result: a single molecule with two chains matching the two models
+
+
+#scenario: pdb with two models, each with 2 chains. user selected read-fragments
+#result: a single molecule with two chains matching the two models, and no chaisn from the chains
+
+
+#scenario: pdb with two models, each with 2 chains. user selected read-fragments, use-chains
+#result: a single molecule with 4 chains?
