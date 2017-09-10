bcdcfb8761a760abb545e8f7ff5fa53faef9f148
devorawitty <devorawitty@gmail.com>
2017-09-10 11:30:30 +0300

    MoleculeFactory missed bug

--- python/csm/calculations/trivial.py
+++ python/csm/calculations/trivial.py
@@ -2,14 +2,14 @@ from csm.fast import CythonPermuter
 from csm.calculations.basic_calculations import CSMState, process_results
 from csm.calculations.constants import MAXDOUBLE
 from csm.calculations.exact_calculations import csm_operation
-from csm.molecule.molecule import Molecule
+from csm.molecule.molecule import Molecule, MoleculeFactory
 
 
 def trivial_calculation(op_type, op_order, molecule, use_chains=True, *args, **kwargs):
     if molecule.chains and use_chains:
         best = CSMState(molecule=molecule, op_type=op_type, op_order=op_order, csm=MAXDOUBLE)
         chain_permutations = []
-        dummy = Molecule.dummy_molecule_from_size(len(molecule._chains), molecule.chain_equivalences)
+        dummy = MoleculeFactory.dummy_molecule_from_size(len(molecule._chains), molecule.chain_equivalences)
         permuter = CythonPermuter(dummy, op_order, op_type, keep_structure=False, precalculate=False)
         for state in permuter.permute():
             chain_permutations.append(list(state.perm))
