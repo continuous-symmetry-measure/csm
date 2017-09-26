91f7adcc894a846574fc7fbc0c2d793c47986ce5
devorawitty <devorawitty@gmail.com>
2017-09-26 13:15:12 +0300

    added result string to result json

--- python/csm/input_output/writers.py
+++ python/csm/input_output/writers.py
@@ -1,6 +1,7 @@
 import json
 
 from csm.input_output.formatters import format_CSM
+import io
 
 __author__ = 'YAEL'
 
@@ -17,7 +18,7 @@ def non_negative_zero(number):
         return number
 
 
-def json_results(result, dictionary_args):
+def json_results(result, dictionary_args, result_string):
     json_dict={"Result":
         {
         "molecule": result.molecule.to_json(),
@@ -25,6 +26,7 @@ def json_results(result, dictionary_args):
         "op_type": result.op_type,
         "csm": result.csm,
 
+        "result_string":result_string,
         "perm": result.perm,
         "dir": list(result.dir),
         "d_min": result.d_min,
@@ -33,12 +35,69 @@ def json_results(result, dictionary_args):
         "perm_count": result.perm_count,
         "formula_csm": result.formula_csm,
         "normalized_molecule_coords": [list(i) for i in result.normalized_molecule_coords],
-        "normalized_symmetric_structure": [list(i) for i in result.normalized_symmetric_structure]
+        "normalized_symmetric_structure": [list(i) for i in result.normalized_symmetric_structure],
              }
            }
     with open(dictionary_args['out_file_name'], 'w', encoding='utf-8') as f:
         json.dump(json_dict, f)
 
+
+def _print_results(f, result, dictionary_args):
+    f.write("%s: %.4lf\n" % (dictionary_args['op_name'], abs(result.csm)))
+    f.write("SCALING FACTOR: %7lf\n" % non_negative_zero(result.d_min))
+
+    # print CSM, initial molecule, resulting structure and direction according to format specified
+    try:
+        percent_structure = check_perm_structure(result.molecule, result.perm)
+        print("The permutation found maintains",
+              str(round(percent_structure * 100, 2)) + "% of the original molecule's structure")
+
+    except ValueError:
+        print(
+            "The input molecule does not have bond information and therefore conservation of structure cannot be measured")
+
+    falsecount, num_invalid, cycle_counts = check_perm_cycles(result.perm, dictionary_args['op_order'],
+                                                              dictionary_args['op_type'])
+    if falsecount > 0 or dictionary_args['calc_type'] == 'approx':
+        print("The permutation found contains %d invalid %s. %.2lf%% of the molecule's atoms are in legal cycles" % (
+        falsecount, "cycle" if falsecount == 1 else "cycles",
+        100 * (len(result.molecule) - num_invalid) / len(result.molecule)))
+        for cycle_len in sorted(cycle_counts):
+            valid = cycle_len == 1 or cycle_len == dictionary_args['op_order'] or (
+            cycle_len == 2 and dictionary_args['op_type'] == 'SN')
+            count = cycle_counts[cycle_len]
+            print("There %s %d %s %s of length %d" % (
+            "is" if count == 1 else "are", count, "invalid" if not valid else "", "cycle" if count == 1 else "cycles",
+            cycle_len))
+
+    if dictionary_args['format'].lower() == "csm":
+        print_output_csm(f, result, dictionary_args)
+    else:
+        print_output_ob(f, result, dictionary_args, dictionary_args, dictionary_args)
+
+    # print local CSM
+
+    if dictionary_args['print_local']:
+        sum = 0
+        f.write("\nLocal CSM: \n")
+        size = len(result.molecule.atoms)
+        for i in range(size):
+            sum += result.local_csm[i]
+            f.write("%s %7lf\n" % (result.molecule.atoms[i].symbol, non_negative_zero(result.local_csm[i])))
+        f.write("\nsum: %7lf\n" % sum)
+
+    # print chirality
+    if dictionary_args['op_type'] == 'CH':
+        if result.op_type == 'CS':
+            f.write("\n MINIMUM CHIRALITY WAS FOUND IN CS\n\n")
+        else:
+            f.write("\n MINIMUM CHIRALITY WAS FOUND IN S%d\n\n" % result.op_order)
+
+    # print permutation
+    f.write("\n PERMUTATION:\n\n")
+    for i in result.perm:
+        f.write("%d " % (i + 1))
+    f.write("\n")
 
 def print_results(result, dictionary_args):
     """
@@ -48,64 +107,25 @@ def print_results(result, dictionary_args):
     :param dictionary_args: Calculation arguments to CSM
     :param dictionary_args: Output arguments to CSM
     """
-    if dictionary_args['json_output']:
-        json_results(result, dictionary_args)
-        return
+
 
     if dictionary_args['calc_type']== 'just_perms':
         print("NUMBER OF PERMUTATIONS: %5.4g" % result)
         return
+
+    if dictionary_args['json_output']:
+        result_io=io.StringIO()
+        _print_results(result_io, result, dictionary_args)
+        result_string=result_io.getvalue()
+        result_io.close()
+        json_results(result, dictionary_args, result_string)
+        return
+
+
+
     with open(dictionary_args['out_file_name'], 'w', encoding='utf-8') as f:
-        f.write("%s: %.4lf\n" % (dictionary_args['op_name'], abs(result.csm)))
-        f.write("SCALING FACTOR: %7lf\n" % non_negative_zero(result.d_min))
+        _print_results(f, result, dictionary_args)
 
-        # print CSM, initial molecule, resulting structure and direction according to format specified
-        try:
-            percent_structure = check_perm_structure(result.molecule, result.perm)
-            print("The permutation found maintains",
-              str(round(percent_structure * 100, 2)) + "% of the original molecule's structure")
-
-        except ValueError:
-            print("The input molecule does not have bond information and therefore conservation of structure cannot be measured")
-
-        falsecount, num_invalid, cycle_counts = check_perm_cycles(result.perm, dictionary_args['op_order'], dictionary_args['op_type'])
-        if falsecount > 0 or dictionary_args['calc_type']=='approx':
-            print("The permutation found contains %d invalid %s. %.2lf%% of the molecule's atoms are in legal cycles" %(falsecount, "cycle" if falsecount==1 else "cycles", 100*(len(result.molecule)-num_invalid) /len(result.molecule) ))
-            for cycle_len in sorted(cycle_counts):
-                valid= cycle_len==1 or cycle_len==dictionary_args['op_order'] or (cycle_len==2 and dictionary_args['op_type']=='SN')
-                count=cycle_counts[cycle_len]
-                print("There %s %d %s %s of length %d" % ("is" if count==1 else "are", count, "invalid" if not valid else "", "cycle" if count==1 else "cycles", cycle_len))
-
-
-        if dictionary_args['format'].lower() == "csm":
-            print_output_csm(f, result, dictionary_args)
-        else:
-            print_output_ob(f, result, dictionary_args, dictionary_args, dictionary_args)
-
-
-        # print local CSM
-
-        if dictionary_args['print_local']:
-            sum = 0
-            f.write("\nLocal CSM: \n")
-            size = len(result.molecule.atoms)
-            for i in range(size):
-                sum += result.local_csm[i]
-                f.write("%s %7lf\n" % (result.molecule.atoms[i].symbol, non_negative_zero(result.local_csm[i])))
-            f.write("\nsum: %7lf\n" % sum)
-
-        # print chirality
-        if dictionary_args['op_type'] == 'CH':
-            if result.op_type == 'CS':
-                f.write("\n MINIMUM CHIRALITY WAS FOUND IN CS\n\n")
-            else:
-                f.write("\n MINIMUM CHIRALITY WAS FOUND IN S%d\n\n" % result.op_order)
-
-        # print permutation
-        f.write("\n PERMUTATION:\n\n")
-        for i in result.perm:
-            f.write("%d " % (i + 1))
-        f.write("\n")
 
 
 def print_output_csm(f, result, calc_args):
