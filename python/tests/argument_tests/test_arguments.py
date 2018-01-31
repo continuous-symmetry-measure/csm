4020239b43e23bd638cedca2f051074fbfa7ec8e
devora <devorawitty@gmail.com>
2018-01-31 11:31:48 +0200

    change tests to work with the new CSM

--- python/tests/argument_tests/test_arguments.py
+++ python/tests/argument_tests/test_arguments.py
@@ -1,5 +1,5 @@
 import pytest
-from csm.main.csm_run_old import run
+from csm.main.csm_run import run
 
 class Runner:
     def run_args(self, args_str):
@@ -10,81 +10,81 @@ class Runner:
 
 class TestExact(Runner):
     def test_plain(self):
-        self.run_args(r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt")
+        self.run_args(r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt")
 
     @pytest.mark.parametrize("run_str",[
-        r"ch C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --sn-max 6",
-        r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2RLA-3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --remove-hy",
-        r"c3 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --use-mass",
-        r"s8 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --ignore-sym",
-        r"s6 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --read-fragments",
-        r"s2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2RLA-3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --use-sequence",
-        r"ci C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --use-chains",
-        r"c4 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --babel-bond",
+        r"exact ch --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --sn-max 6",
+        r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2RLA-3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt --remove-hy",
+        r"exact c3 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --use-mass",
+        r"exact s8 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --ignore-sym",
+        r"exact s6 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --read-fragments",
+        r"exact s2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2RLA-3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt --use-sequence",
+        r"exact ci --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --use-chains",
+        r"exact c4 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --babel-bond",
     ])
     def test_input_args(self, run_str):
         self.run_args(run_str)
     def test_exact_args(self):
-        self.run_args(r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --keep-structure")
-        self.run_args(r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --no-constraint")
+        self.run_args(r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --keep-structure")
+        self.run_args(r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --no-constraint")
 
     @pytest.mark.parametrize("run_str",
                              [
-        r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --print-denorm",
-        r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --output-branches",
-        r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --output-perms perms.csv",
-        r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --print-local",
-        r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --json-output",
+        r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --print-denorm",
+        r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --output-branches",
+        r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --output-perms perms.csv",
+        r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --print-local",
+        r"exact c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt --json-output",
     ])
     def test_output_args(self, run_str):
         self.run_args(run_str)
 
 class TestApprox(Runner):
     def test_plain(self):
-        self.run_args(r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx")
-        self.run_args(r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --print-approx")
+        self.run_args(r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt ")
+        self.run_args(r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --print-approx")
     @pytest.mark.parametrize("run_str",
                              [
 
-                                 r"ch C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --sn-max 6",
-                                 r"ci C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --use-chains",
-                                 r"s2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --use-sequence",
-                                 r"s6 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --read-fragments",
+                                 r"approx ch --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --sn-max 6",
+                                 r"approx ci --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --use-chains",
+                                 r"approx s2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --use-sequence",
+                                 r"approx s6 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --read-fragments",
 
                              ])
     def test_input_args(self, run_str):
         self.run_args(run_str) 
     @pytest.mark.parametrize("run_str",
                              [
-                                 r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --detect-outliers",
-                                 r"c3 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --no-orthogonal",
-                                 r"c5 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --detect-outliers --no-orthogonal",
-                                 r"cs C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --fibonacci 20",
-                                 r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --use-best-dir",
-                                 r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --many-chains",
-                                 r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --greedy",
-                                 r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --fibonacci 20 --selective 3",
-                                 r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --statistics C:\Users\devora\Sources\temp\csm_testoutputstats.txt",
-                                 r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --statistics C:\Users\devora\Sources\temp\csm_testoutputstats.txt --polar",
+                                 r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --detect-outliers",
+                                 r"approx c3 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --no-orthogonal",
+                                 r"approx c5 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --detect-outliers --no-orthogonal",
+                                 r"approx cs --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --fibonacci 20",
+                                 r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --use-best-dir",
+                                 r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --many-chains",
+                                 r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --greedy",
+                                 r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --fibonacci 20 --selective 3",
+                                 r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --statistics C:\Users\devora\Sources\temp\csm_testoutputstats.txt",
+                                 r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --statistics C:\Users\devora\Sources\temp\csm_testoutputstats.txt --polar",
 
                              ])
     def test_approx_args(self, run_str):
         self.run_args(run_str)
         
     def test_output_args(self):
-        self.run_args(r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --approx --json-output")
+        self.run_args(r"approx c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --json-output")
 
 class TestTrivial(Runner):
     def test_plain(self):
-        self.run_args(r"c2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol C:\Users\devora\Sources\temp\csm_testoutput.txt --trivial")
+        self.run_args(r"trivial c2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\just-one-mol.mol --output C:\Users\devora\Sources\temp\csm_testoutput.txt ")
     
     @pytest.mark.parametrize("run_str", [
 
-        r"ch C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --trivial --sn-max 6",
-        r"c4 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --trivial --babel-bond",
-        r"ci C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --trivial --use-chains",
-        r"s2 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --trivial --use-sequence",
-        r"s6 C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb C:\Users\devora\Sources\temp\csm_testoutput.txt --trivial --read-fragments",
+        r"trivial ch --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --sn-max 6",
+        r"trivial c4 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --babel-bond",
+        r"trivial ci --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --use-chains",
+        r"trivial s2 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --use-sequence",
+        r"trivial s6 --input C:\Users\devora\Sources\csm\python\tests\unit_tests\molecules_for_tests\2m7w-q3.pdb --output C:\Users\devora\Sources\temp\csm_testoutput.txt  --read-fragments",
     ])
     def test_input_args(self, run_str):
         self.run_args(run_str)
