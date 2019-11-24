c158e17046696721e09b757c3d43f44ddc351e5c
devora <devorawitty@gmail.com>
2019-11-24 14:44:24 +0200

    fix bat file copy destination

--- python/rebuild.bat
+++ python/rebuild.bat
@@ -1,9 +1,4 @@
 python setup.py prepare
 python setup.py build_ext
-PUSHD
 ECHO copying pyd files
-cd build
-   for /r %%a in (*.pyd) do (
-     COPY "%%a" "%~dp0%%~nxa"
-   )
-POPD
+for /R .\build %%f in (*.pyd) do copy %%f .\csm
