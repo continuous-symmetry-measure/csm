c96dbcf17524dde2685300aee76e4bdeb8d25a38
Itay Zandbank <itay@chelem.co.il>
2022-06-09 12:01:29 +0300

    Get ready for more openbabel mess

---
+++ src/install_requirements.py
@@ -0,0 +1,20 @@
+# Install the requirements
+
+def get_openbabel_dependency():
+    # Returns the openbabel dependency:
+    # On linux, return openbabel==3.1.1.1
+    # On windows, Python 3.7 return openbael==3.1.1
+    # On windows, other Python versions, return the URL
+    pass
+
+def get_openbabel_url():
+    # Returns the the openbabel installation URL
+    pass
+
+def main():
+    # Install the proper openbabel, then install requirements.windows.txt on windows, requirements.linux.txt on linux
+    # use os.system('pip install...') to install something.
+    pass
+
+if __name__ == '__main__':
+    main()
