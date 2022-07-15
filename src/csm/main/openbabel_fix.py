15ea69b2924bb933f7ae54d81755d6aa94b6e2c0
Itay Zandbank <itay@chelem.co.il>
2022-07-15 09:23:46 +0300

    try_openbabel

--- src/csm/main/openbabel_fix.py
+++ src/csm/main/openbabel_fix.py
@@ -10,8 +10,10 @@ import os
 
 openbabel_prepared = False
 def prepare_openbabel():
+    print("prepare_openbabel here")
     global openbabel_prepared
     if openbabel_prepared:
+        print("openbabel already prepared")
         return
 
     if sys.platform != 'win32': # Only Windows seem to require this
@@ -20,14 +22,18 @@ def prepare_openbabel():
     if sys.version_info < (3, 8): # No need for this in Python 3.7 or earlier
         return
 
+    print("We need to prepare")
     # Look for OpenBabel on the path
     paths = os.environ['PATH'].split(';') # Windows only here, no need to look for other separators
     openbabel_paths = [p for p in paths if 'openbabel' in p.lower()]
-
+    print("All potential openbabel paths: ", openbabel_paths)
     if not openbabel_paths:
         raise ValueError("Can't locate OpenBabel in the PATH, please add it")
 
     # We are not picky here, just add all these directories as candidates
     for path in openbabel_paths:
+        print(f"Adding {path} to DLL loading path")
         os.add_dll_directory(path)
+        print("OK")
     openbabel_prepared = True
+    print("Done")
